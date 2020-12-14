98b82254c529bbb5fd79355979c48f97ef017c7e
BILLY Maxime <59318604+Oseliurs@users.noreply.github.com>
2020-12-14 23:48:41 +0100

    Update NASSync.sh

--- NASSync.sh
+++ NASSync.sh
@@ -9,11 +9,11 @@ sudo mkdir /mnt/ftp
 sudo curlftpfs "master":""@10.0.0.70 /mnt/ftp/
 
 echo "Copy New Movies ..."
-cp -Ruv /mnt/ftp/Volume_2/My\ Movies /home/jellyfin/Documents/Jellyfin-Library/My\ Movies/
+sudo rsync -aP --delete /mnt/ftp/Volume_2/My\ Movies /home/jellyfin/Documents/Jellyfin-Library/
 echo "Copy New Series ..."
-cp -Ruv /mnt/ftp/Volume_2/My\ Series /home/jellyfin/Documents/Jellyfin-Library/My\ Series/
+sudo rsync -aP --delete /mnt/ftp/Volume_2/My\ Series /home/jellyfin/Documents/Jellyfin-Library/
 echo "Copy New Music ..."
-cp -Ruv /mnt/ftp/Volume_2/My\ Music /home/jellyfin/Documents/Jellyfin-Library/My\ Music/
+sudo rsync -aP --delete /mnt/ftp/Volume_2/My\ Music /home/jellyfin/Documents/Jellyfin-Library/
 echo "Done !!!"
 
 echo "Unmouting ..."
