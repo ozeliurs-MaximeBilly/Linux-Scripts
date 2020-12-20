6862a576006c07821917ff897cab7a6d24998f51
BILLY Maxime <59318604+Oseliurs@users.noreply.github.com>
2020-12-20 22:33:01 +0100

    fix colors -e and -r in cp

--- PullWebsites.sh
+++ PullWebsites.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
-echo -e "\e[33mUpdating Services ...\e[0m"
+echo "\e[33mUpdating Services ...\e[0m"
 sudo apt-get update
 sudo apt-get install git -y
 sudo apt-get install apache2 -y
-echo -e "\e[32mServices Updated.\e[0m"
+echo "\e[32mServices Updated.\e[0m"
 
-echo -e "\e[33mCloning Websites to websites ...\e[0m"
+echo "\e[33mCloning Websites to websites ...\e[0m"
 mkdir /home/ubuntu/
 cd /home/ubuntu/
-echo -e "\e[33mCleaning up previous pull ...\e[0m"
+echo "\e[33mCleaning up previous pull ...\e[0m"
 rm /home/ubuntu/websites -r
-echo -e "\e[33mPulling ...\e[0m"
+echo "\e[33mPulling ...\e[0m"
 git clone https://github.com/Oseliurs/websites.git
-echo -e "\e[32mWebsite Cloned !!\e[0m"
+echo "\e[32mWebsite Cloned !!\e[0m"
 
-echo -e "\e[33mUpdating Contents ...\e[0m"
-echo -e "\e[33mEmptying Website Active Directory ...\e[0m"
+echo "\e[33mUpdating Contents ...\e[0m"
+echo "\e[33mEmptying Website Active Directory ...\e[0m"
 sudo rm /var/www/beta -r
 sudo rm /var/www/main -r
-echo -e "\e[33mCopying new version ...\e[0m"
-sudo cp /home/ubuntu/websites/main /var/www/main
-sudo cp /home/ubuntu/websites/beta /var/www/beta
-echo -e "\e[32mContents Updated !!!\e[0m"
+echo "\e[33mCopying new version ...\e[0m"
+sudo cp -r /home/ubuntu/websites/main /var/www/main
+sudo cp -r /home/ubuntu/websites/beta /var/www/beta
+echo "\e[32mContents Updated !!!\e[0m"
