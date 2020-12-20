5207815def75023abf22e56f125661b1c7bb78e2
BILLY Maxime <59318604+Oseliurs@users.noreply.github.com>
2020-12-20 22:29:49 +0100

    Update PullWebsites.sh

--- PullWebsites.sh
+++ PullWebsites.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
-echo -e "\u001b[33mUpdating Services ...\u001b[0m"
+echo -e "\e[33mUpdating Services ...\e[0m"
 sudo apt-get update
 sudo apt-get install git -y
 sudo apt-get install apache2 -y
-echo -e "\u001b[32mServices Updated.\u001b[0m"
+echo -e "\e[32mServices Updated.\e[0m"
 
-echo -e "\u001b[33mCloning Websites to websites ...\u001b[0m"
+echo -e "\e[33mCloning Websites to websites ...\e[0m"
 mkdir /home/ubuntu/
 cd /home/ubuntu/
-echo -e "\u001b[33mCleaning up previous pull ...\u001b[0m"
+echo -e "\e[33mCleaning up previous pull ...\e[0m"
 rm /home/ubuntu/websites -r
-echo "\u001b[33mPulling ...\u001b[0m"
+echo -e "\e[33mPulling ...\e[0m"
 git clone https://github.com/Oseliurs/websites.git
-echo -e "\u001b[32mWebsite Cloned !!\u001b[0m"
+echo -e "\e[32mWebsite Cloned !!\e[0m"
 
-echo -e "\u001b[33mUpdating Contents ...\u001b[0m"
-echo -e "\u001b[33mEmptying Website Active Directory ...\u001b[0m"
+echo -e "\e[33mUpdating Contents ...\e[0m"
+echo -e "\e[33mEmptying Website Active Directory ...\e[0m"
 sudo rm /var/www/beta -r
 sudo rm /var/www/main -r
-echo -e "\u001b[33mMoving new version ...\u001b[0m"
-sudo mv /home/ubuntu/websites/main /var/www/main
-sudo mv /home/ubuntu/websites/beta /var/www/beta
-echo -e "\u001b[32mContents Updated !!!\u001b[0m"
+echo -e "\e[33mCopying new version ...\e[0m"
+sudo cp /home/ubuntu/websites/main /var/www/main
+sudo cp /home/ubuntu/websites/beta /var/www/beta
+echo -e "\e[32mContents Updated !!!\e[0m"
