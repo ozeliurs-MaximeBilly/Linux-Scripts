a3fdd81be5ab0cf1f5b6210f034a491179a12183
BILLY Maxime <59318604+Oseliurs@users.noreply.github.com>
2020-12-22 16:43:46 +0100

    Update PullWebsites.sh

--- PullWebsites.sh
+++ PullWebsites.sh
@@ -1,25 +1,25 @@
-#!/bin/sh
+!/bin/sh
+
+HOME=/home/ubuntu
 
 echo "\e[33mUpdating Services ...\e[0m"
 sudo apt-get update
 sudo apt-get install git -y
-sudo apt-get install apache2 -y
 echo "\e[32mServices Updated.\e[0m"
 
 echo "\e[33mCloning Websites to websites ...\e[0m"
-mkdir /home/ubuntu/
-cd /home/ubuntu/
+cd $HOME
 echo "\e[33mCleaning up previous pull ...\e[0m"
-rm /home/ubuntu/websites -r
+rm $HOME/websites -r
 echo "\e[33mPulling ...\e[0m"
 git clone https://github.com/Oseliurs/websites.git
+rm $HOME/websites/README.md
 echo "\e[32mWebsite Cloned !!\e[0m"
 
 echo "\e[33mUpdating Contents ...\e[0m"
 echo "\e[33mEmptying Website Active Directory ...\e[0m"
-sudo rm /var/www/beta -r
-sudo rm /var/www/main -r
+cd /var/www/
+sudo rm * -r
 echo "\e[33mCopying new version ...\e[0m"
-sudo cp -r /home/ubuntu/websites/main /var/www/main
-sudo cp -r /home/ubuntu/websites/beta /var/www/beta
+sudo cp -r $HOME/websites/* /var/www/
 echo "\e[32mContents Updated !!!\e[0m"
