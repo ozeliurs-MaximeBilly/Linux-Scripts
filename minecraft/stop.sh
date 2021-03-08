b2d1a955477fe3ec9729c375ae452d0897d95c12
BILLY Maxime <ozeliurs@gmail.com>
2021-03-08 11:02:40 +0100

    Create stop.sh

---
+++ minecraft/stop.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Description : "Restart deamon"
+
+if [ "$#" = 0 ];
+then
+	  echo "Arret de tous les serveurs ..."
+
+		for SERV in `ls ~/minecraft/$.mc`
+		do
+		  echo "Arret de $SERV."
+		  screen -R "$SERV" -X stuff "cd ~/minecraft/$SERV/ $(printf "\r")"
+		  screen -R "$SERV" -X stuff "stop $(printf "\r")"
+		done
+
+		sleep 30s
+
+else
+	for SERV in "$@"
+	do
+
+		for SERV in "$@"
+		do
+		  echo "Arret de $SERV."
+		  screen -R "$SERV" -X stuff "cd ~/minecraft/$SERV/ $(printf "\r")"
+		  screen -R "$SERV" -X stuff "stop $(printf "\r")"
+		done
+    
+	done
+fi
