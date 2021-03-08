83d520c18840f0a050fb061559e51690ac2e6ea9
BILLY Maxime <ozeliurs@gmail.com>
2021-03-08 11:32:55 +0100

    Create start.sh

---
+++ minecraft/start.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Description : "Start deamon"
+
+if [ "$#" = 0 ];
+then
+	echo "Démarrage de tous les serveurs ..."
+
+		for SERV in `ls ~/minecraft/*.mc`
+		do
+		  echo "Démarrage de $SERV."
+		  screen -R "$SERV" -X stuff "cd ~/minecraft/$SERV/ $(printf "\r")"
+		  screen -R "$SERV" -X stuff "./start.sh $(printf "\r")"
+		done
+
+		echo "Serveurs en cours de démarrage ..."
+
+else
+	for SERV in "$@"
+	do
+		echo "Démarrage de $@."
+
+		for SERV in "$@"
+		do
+		  echo "Démarrage de $SERV."
+		  screen -R "$SERV" -X stuff "cd ~/minecraft/$SERV/ $(printf "\r")"
+		  screen -R "$SERV" -X stuff "./start.sh $(printf "\r")"
+		done
+
+		echo "Serveurs en cours de démarrage ..."
+	done
+fi
