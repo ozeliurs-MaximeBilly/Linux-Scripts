d431eecd967dc4c21b0542dc9c14784c3fd2ada0
BILLY Maxime <ozeliurs@gmail.com>
2021-03-08 11:00:06 +0100

    Create firstboot.sh

---
+++ minecraft/firstboot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Description : "Cold Start Daemon"
+
+echo "Démarrage de tous les serveurs ..."
+
+for SERV in `ls ~/minecraft/$.mc`
+do
+  echo "Démarrage"
+  screen -S "$SERV" -d -m 
+  screen -R "$SERV" -X stuff "cd ~/minecraft/$SERV/ $(printf "\r")"
+  screen -R "$SERV" -X stuff "./start.sh $(printf "\r")"
+done
+
+echo "Serveurs en cours de démarrage ..."
