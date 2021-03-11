77aac6f67f4f76b749d0550eb3b86cab3bbd657a
BILLY Maxime <ozeliurs@gmail.com>
2021-03-11 21:37:42 +0100

    update to dynamic paths

--- minecraft/backupd.sh
+++ minecraft/backupd.sh
@@ -4,7 +4,8 @@
 
 #define backup folder
 script_path="$(dirname $(readlink -f $0))/"
-backup="$script_path../backup"
+backup_path="$script_path../backup" # ici /root/minecraft/scripts/../backup
+server_path="$script_path../
 
 
 if [ "$#" = 0 ];
@@ -12,14 +13,14 @@ then
 	echo "Backup de tous les serveurs ..."
 
 	echo "Prevention des joueurs ..."
-	for SERV in `ls ~/minecraft/ | grep ".mc"`
+	for SERV in `ls $server_path | grep ".mc"`
 	do
 		screen -R "$SERV" -X stuff " say Redemarrage du serveur pour Backup dans 2 minutes !$(printf "\r")"
 	done
 
 	sleep 120s
 
-	for SERV in `ls ~/minecraft/ | grep ".mc"`
+	for SERV in `ls $server_path | grep ".mc"`
 	do
 		echo "Arret de $SERV."
 		screen -R "$SERV" -X stuff "say Redemarrage imminent. $(printf "\r")"
@@ -29,22 +30,22 @@ then
 
 	sleep 30s
 
-	for SERV in `ls ~/minecraft/ | grep ".mc"`
+	for SERV in `ls $server_path | grep ".mc"`
 	do
 		echo "Backup de $SERV."
-		if [ -d "~/backup/$SERV" ];
+		if [ -d "$backup_path/$SERV" ];
 		then
     		echo "Directory exists."
     	else
-    		mkdir ~/backup/$SERV
+    		mkdir $backup_path/$SERV
     	fi
 
-    	tar -cvpzf "$backup/$SERV/backup-$(date +%F\ -\ %Hh).tar.gz" "~/minecraft/$SERV"
+    	tar -cvpzf "$backup_path/$SERV/backup-$(date +%F\ -\ %Hh).tar.gz" "$server_path/$SERV"
     done
 
     sleep 30s
 
-    for SERV in `ls ~/minecraft/ | grep ".mc"`
+    for SERV in `ls $server_path | grep ".mc"`
     do
     	echo "Démarrage de $SERV."
     	screen -R "$SERV" -X stuff "cd ~/minecraft/$SERV/ $(printf "\r")"
@@ -76,14 +77,14 @@ else
 	for SERV in "$@"
 	do
 		echo "Backup de $SERV."
-		if [ -d "~/backup/$SERV" ];
+		if [ -d "$backup_path/$SERV" ];
 		then
     		echo "Directory exists."
     	else
-    		mkdir ~/backup/$SERV
+    		mkdir $backup_path/$SERV
     	fi
 
-    	tar -cvpzf "$backup/$SERV/backup-$(date +%F\ -\ %Hh).tar.gz" "~/minecraft/$SERV"
+    	tar -cvpzf "$backup/$SERV/backup-$(date +%F\ -\ %Hh).tar.gz" "$backup/minecraft/$SERV"
     done
 
     sleep 30s
